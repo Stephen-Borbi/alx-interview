94c4fa0ed591e88274f0a1a3a7ecf194028fdb19
xorladem <113623998+xorladem@users.noreply.github.com>
2023-07-26 19:23:36 +0000

    pascal triangle import file 😀

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,1 +1,24 @@
-def print_triangle(triangle)
+#!/usr/bin/python3
+"""
+0-pascal_triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of integers
+    representing the Pascal Triangle of n
+    returns empty list if n <= 0
+    """
+    y = []
+    if n <= 0:
+        return y
+    y = [[1]]
+    for z in range(1, n):
+        temp = [1]
+        for j in range(len(y[z - 1]) - 1):
+            curr = y[z - 1]
+            temp.append(y[z - 1][j] + y[z - 1][j + 1])
+        temp.append(1)
+        y.append(temp)
+    return y
